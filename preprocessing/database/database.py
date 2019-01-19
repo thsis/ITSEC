d2cbf641d891ba15833b08cb2a335f5c11555b11
Thomas Siskos <thomas.siskos91@gmail.com>
2019-01-19 14:49:47 +0100

    add query function to database.py

--- preprocessing/database/database.py
+++ preprocessing/database/database.py
@@ -1,3 +1,4 @@
+import os
 import psycopg2
 from config import config
 from web3.auto import w3
@@ -109,3 +110,23 @@ if __name__ == "__main__":
         except Exception as e:
             print(e)
             continue
+
+
+def query(block):
+    with open(os.path.join("sql", "query", "extract_info.sql")) as q:
+        sql = q.read()
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        cur.execute(sql.format(block))
+        rows = cur.fetchall()
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return rows
