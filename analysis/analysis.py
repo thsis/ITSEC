f17c00cebb5b83bcd65c9603a9b5f2c43258ffd9
Thomas Siskos <thomas.siskos91@gmail.com>
2019-01-21 11:50:43 +0100

    Add plots for powerlaw fit. Add checkpoint for analysis.py

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,14 +1,14 @@
 import os
+import powerlaw
 import networkx as nx
 import numpy as np
 import pandas as pd
-from scipy.stats import pearsonr
 from matplotlib import pyplot as plt
 
 
 data_path = os.path.join(".", "data", "ethereum_data.csv")
 general_data_path = os.path.join(".", "data", "export-EtherPrice.csv")
-outdir = os.path.join(".", "data")
+outdir = os.path.join(".", "analysis")
 
 # Load custom dataset
 data = pd.read_csv(data_path)
@@ -49,21 +49,6 @@ daily = daily[["sender", "receiver", "users", "value", "val"]]
 
 # =============================================================================
 # Draw the graph
-n_transactions = users.user.value_counts()
-major_players = n_transactions[n_transactions > 2000].index.values.tolist()
-data["is_major_sender"] = data.sender.isin(major_players)
-data["is_major_receiver"] = data.receiver.isin(major_players)
-
-graph_data = data[data.is_major_sender | data.is_major_receiver]
-sample = graph_data.sample(1500)
-
-G = nx.from_pandas_edgelist(df=sample[["sender", "receiver", "value"]],
-                            source="sender",
-                            target="receiver",
-                            edge_attr="value")
-G.add_nodes_from(nodes_for_adding=sample.sender.tolist())
-nx.draw(G, with_labels=False)
-
 sender = set(data.sender.values)
 receiver = set(data.receiver.values)
 interactors = sender.intersection(receiver)
@@ -90,21 +75,6 @@ nx.draw(G, with_labels=False, ax=ax)
 ax.set_title("Graph of 1000 randomly drawn Interactors")
 fig.savefig(os.path.join(outdir, "graph-subsample-1000.png"))
 
-degrees = interactor_data.receiver.value_counts() +
-degrees.sum()
-
-
-degrees_receiver = interactor_data.receiver.value_counts()
-degrees_sender = interactor_data.sender.value_counts()
-
-degrees_sender.shape
-degrees_receiver.shape
-
-(degrees_sender + degrees_receiver)
-intersection
-
-
-
 fig, ax = plt.subplots(1, figsize=(15, 15))
 G = nx.from_pandas_edgelist(df=interactor_data,
                             source="sender",
@@ -113,3 +83,60 @@ G = nx.from_pandas_edgelist(df=interactor_data,
 G.add_nodes_from(nodes_for_adding=interactor_data.sender.tolist())
 nx.draw(G, with_labels=False, ax=ax)
 ax.set_title("Graph of all Interactors")
+fig.savefig(os.path.join(outdir, "graph-full.png"))
+
+# Calculate Network metrics
+idx = pd.Index(interactors)
+outgoing = interactor_data.sender.value_counts().reindex(idx, fill_value=0)
+ingoing = interactor_data.receiver.value_counts().reindex(idx, fill_value=0)
+
+edges = pd.concat([outgoing.rename("outgoing"), ingoing.rename("ingoing")],
+                  axis=1, levels=["ingoing", "outgoing"])
+edges["total"] = outgoing + ingoing
+
+# Centrality
+triangles = nx.triangles(G)
+edges["triangles"] = pd.Series(triangles).reindex(idx, fill_value=0)
+edges["C_in"] = 2*edges["triangles"] / (edges["ingoing"]*(edges["ingoing"]-1))
+edges["C_out"] = 2*edges["triangles"] / (edges["outgoing"]*(edges["outgoing"]-1))
+edges["C_total"] = 2*edges["triangles"] / (edges["total"]*(edges["total"]-1))
+
+avg_cluster = edges[["C_in", "C_out", "C_total"]].replace(np.inf, 0).fillna(0).mean()
+
+# Fit power law
+fig, ax = plt.subplots(1)
+
+
+def plot_dist(data, ax):
+    """Compare theoretical and empirical powerlaw distribution."""
+    data = data[data > 0]
+    fit = powerlaw.Fit(data, discrete=True)
+    fit.plot_pdf(color='b', linewidth=2, ax=ax, label="PDF")
+    fit.power_law.plot_pdf(color='b', linestyle="--", ax=ax,
+                           label="Power Law fit PDF")
+
+    fit.plot_ccdf(color='r', linewidth=2, ax=ax, label="CCDF")
+    fit.power_law.plot_ccdf(color='r', linestyle="--", ax=ax,
+                            label="Power Law fit CCDF")
+
+    ax.set_ylabel(r"p")
+    ax.legend(loc="lower left")
+
+
+fig, axes = plt.subplots(ncols=3, figsize=(20, 5))
+titles = ["Transactions sent", "Transactions received", "Total transactions"]
+datalist = [edges.outgoing, edges.ingoing, edges.total]
+for title, ax, data in zip(titles, axes, datalist):
+    plot_dist(data.values, ax)
+    ax.set_title(title)
+
+fig.savefig(os.path.join(outdir, "power-law-fit.png"))
+
+# Playground Area
+# Degree centrality
+deg_cent = nx.degree_centrality(G)
+edges["deg_centrality"] = pd.Series(deg_cent).reindex(idx)
+
+# Between centrality
+bet_cent = nx.betweenness_centrality(G)
+edges["betw_centrality"] = pd.Series(bet_cent).reindex(idx)
