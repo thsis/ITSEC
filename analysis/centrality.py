24390ac936a5341e6ab79ac4afde7b1177d2d702
Thomas Siskos <thomas.siskos91@gmail.com>
2019-03-16 17:22:34 +0100

    Add checkpoint. Move centrality computation to remote server.

---
+++ analysis/centrality.py
@@ -0,0 +1,67 @@
+import os
+import networkx as nx
+import pandas as pd
+from sys import argv
+
+_, sample_size = argv
+data_path = os.path.join(".", "data", "ethereum-data.zip")
+general_data_path = os.path.join(".", "data", "export-EtherPrice.csv")
+outdir = os.path.join(".", "analysis")
+
+data = pd.read_csv(data_path)
+data.inception_time = pd.to_datetime(data.inception_time,
+                                     unit="s")
+
+data["day"] = data.inception_time.dt.date
+# Convert to ether
+data["value"] = data["value"] / 10**18
+
+# Load general dataset
+general_data = pd.read_csv(general_data_path,
+                           names=["date", "timestamp", "val"], skiprows=[0])
+general_data.date = pd.to_datetime(general_data.date)
+general_data["day"] = general_data.date.dt.date
+
+# Merge dataframes
+data = pd.merge(data, general_data, how="left", on="day")
+receivers = data.loc[:, ["receiver", "day"]]
+receivers.columns = ["user", "day"]
+senders = data.loc[:, ["sender", "day"]]
+senders.columns = ["user", "day"]
+
+# =============================================================================
+# Draw the graph
+sender = set(data.sender.values)
+receiver = set(data.receiver.values)
+interactors = sender.intersection(receiver)
+
+loc = data.sender.isin(interactors) & data.receiver.isin(interactors)
+interactor_data = data.loc[loc, ["sender", "receiver", "value"]]
+
+print("Initialize graph")
+sample = interactor_data.sample(10000)
+G = nx.from_pandas_edgelist(df=sample,
+                            source="sender",
+                            target="receiver",
+                            edge_attr="value")
+G.add_nodes_from(nodes_for_adding=sample.sender.tolist())
+
+print("Compute Centrality-Measures")
+deg_cent = nx.degree_centrality(G)
+print(deg_cent.describe()())
+bet_deg_cent = nx.betweenness_centrality(G)
+closeness_cent = nx.closeness_centrality(G)
+node_clustering = nx.clustering(G)
+
+avg_clustering = nx.average_clustering(G)
+print("Average clustering coefficient:", avg_clustering)
+
+centrality = pd.DataFrame({
+    "degree centrality": deg_cent,
+    "betweenness centrality": bet_deg_cent,
+    "closeness centrality": closeness_cent,
+    "local clustering": node_clustering
+})
+
+print("Save to .csv")
+centrality.to_csv(os.path.join(outdir, "centrality_"+str(sample_size)+".csv"))
